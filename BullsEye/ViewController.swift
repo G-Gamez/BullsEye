0b605098288a639c23d60fa46e6a6753f9f5329e
Gamez <gonzalo.gamez@myhisd.net>
2019-11-05 11:07:04 -0600

    Pushing to Github

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,6 +15,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert(){
+
+        let alert = UIAlertController(title: "Hello,World message", message: "This is my first app" , preferredStyle:.alert)
+
+        let action = UIAlertAction(title: "Nice", style: <#T##UIAlertAction.Style#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func knockknock(){
+        
+    
+
+        let alert = UIAlertController(title: "Knock Knock", message: "Who's There?" , preferredStyle:.alert)
+        
+        let action = UIAlertAction(title: "Little old lady", style: <#T##UIAlertAction.Style#>, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>)
+        
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+     }
 
 }
-
